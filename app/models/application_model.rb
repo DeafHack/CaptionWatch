785fc74e50da6e13fb7b600bf0a2176191124e04
William Albright <ping@ewilli.am>
2013-08-19 14:31:49 -0700

    set up appmodel

--- app/models/application_model.rb
+++ app/models/application_model.rb
@@ -1,7 +1,7 @@
 class ApplicationModel < ActiveResource::Base
 
-	self.site = "http://localhost:3001"
+	self.site = "http://captionwatch-api.dev"
 	self.user = "admin"
-	self.password = "secret"
+	# self.password = "secret"
 
 end
